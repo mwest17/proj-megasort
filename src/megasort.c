816de2402e6e1ec9066c0faa2186695a6eac6e79
Matthew West <matthewjwest53@gmail.com>
2026-02-13 22:50:48 -0500

    Sort of good merge sort function

--- src/megasort.c
+++ src/megasort.c
@@ -1,16 +1,29 @@
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <stdio.h>
 
+char* splitProg;
+char* sortProg;
+char* mergeProg;
 
+long maxLines;
 
-void splitCall() {
-    
+void splitCall(char* inputFile, long lines) {
+    pid_t childPID = fork();
+
+    if (childPID == 0) {
+        execl(splitProg, inputFile, lines, (char *) NULL);
+    } else {
+        waitpid(childPID);
+    }
 }
 
 
-void sortCall() {
-
-
+void sortCall(char* inputFileName, char* outPutFileName) {
+    
+    
+    // Stores to file
 }
 
 
@@ -20,24 +33,58 @@ void mergeCall() {
     // Wait if parent
 }
 
-void mergeSort() { // input file handle, isChild flag, # Lines, 
-    // If len of file <= max_lines
-        // Sort file
-        // return
-    
-    // Create a child process (fork)
-    // Store if parent or child
+void mergeSort(char* inputFileName, char* outputFileName, long lineCount) { //(FILE* inputFile, char* inputFileName, FILE* outputFile, long lineCount) {
+    // Base Case: We can sort directly
+    if (lineCount <= maxLines) {
+        sortCall(inputFileName, outputFileName);
+        return;
+    }
+
+    // lineCount > maxLines so recursively sort files
+
     // Split file 
-    // Open both smaller files
-    // Both recursively call mergeSort() on their corresponding files
-        // appropriate isChild flag, # Lines / 2 (ensure if odd # lines works), 
+    long newLineCount = (lineCount + 2 - 1) / 2; // NEED TO HANDLE ODD LENGTHS
+    splitCall(inputFileName, newLineCount);
     
-    // If child, kill process
-    // If parent, wait until child is finished, 
-        //then merge child's file 
-        // Must save stdout output to a file
-        // Then if we are child too, kill
-        // Else return (might be fine to just return no matter what)
+    
+    // Create new output Files
+    FILE* childOutput = fopen("output1.txt", "w");
+    FILE* parentOutput = fopen("output0", "w");
+
+    FILE* childInput = fopen("part1.txt", "r");
+    FILE* parentInput = fopen("part0.txt", "r");
+
+    // Go into new directory (so workspace isn't cluttered)
+    pid_t PID = getpid();
+    char newDir[8];
+    sprintf(newDir, "%d", PID); 
+    mkdir(newDir);
+    chdir(newDir);
+
+    // Create a child process (fork)
+    pid_t childPID = fork() == 0;
+
+    if (childPID == 0) { // Child process
+        // Both recursively call mergeSort() on their corresponding files
+        mergeSort(childInput, childOutput, newLineCount);
+    } else {
+        mergeSort(parentInput, parentOutput, newLineCount);
+    }
+
+    chdir("..");
+
+    // If parent, then we need to merge files
+    if (childPID != 0) {
+        waitpid(childPID); // Must wait for child to be done sorting
+        mergeCall(parentOutput, childOutput, outputFile); // Merge files together
+    }
+
+    fclose(childInput);
+    fclose(parentInput);
+    fclose(parentOutput);
+    fclose(childOutput);
+
+    return;
 }
 
 
@@ -46,18 +93,22 @@ int main(int argc, char* argv[]) {
         printf("Invalid number of arguments: %d\n", argc);
     } 
 
-    char* splitProg = argv[1];
-    char* sortProg = argv[2];
-    char* mergeProg = argv[3];
+    splitProg = argv[1]; // Append path to current directory to front
+    sortProg = argv[2];
+    mergeProg = argv[3];
 
-    char* inputFile = argv[4];
-    long maxLines = stoi(argv[5]);
-    char* outputFile = argv[6];
+    char* inputPath = argv[4];
+    maxLines = stoi(argv[5]);
+    char* outputPath = argv[6];
 
+    FILE* inputFile = fopen(inputPath, "r");
+    FILE* outputFile = fopen(outputPath, "w");
+
+    long lineCount; // TODO**
     // Find number of lines in file
         // This way costly io to read # lines is only taken once
 
-    mergeSort();
+    mergeSort(inputFile, inputPath, outputFile, lineCount);
 
     return 0;
 }
